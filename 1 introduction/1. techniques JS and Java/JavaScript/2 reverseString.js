12c0a81ec672327b8f5d10cbfc31617e0284e3bb
aditya bhojane <adityabhojane66@gmail.com>
2024-06-13 07:48:43 +0530

    add reverse string new solution

--- 1 introduction/1. techniques JS and Java/JavaScript/2 reverseString.js
+++ 1 introduction/1. techniques JS and Java/JavaScript/2 reverseString.js
@@ -16,3 +16,21 @@ function Reverse(arr){
 }
 
 Reverse(['i','P','h','o','n','e'])
+
+
+
+
+// leetcode
+
+var reverseString = function(arr) {
+    let n = arr.length;
+    let i = 0;
+    let j = n-1;
+    while(i<j){
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        i++
+        j--
+    }
+};
